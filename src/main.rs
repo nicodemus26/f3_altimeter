53e46ba8e9a572990f1787bbc9002ff4fac90f3b
Nicodemus E Paradiso <nicodemus26@gmail.com>
2018-09-16 11:19:46 -0700

    Get ready to interact with i2c dev

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 //! Blinks an LED
 #![deny(unsafe_code)]
-#![deny(warnings)]
 #![no_std]
 #![no_main]
 
@@ -13,8 +12,13 @@ extern crate panic_semihosting;
 use f3::hal::delay::Delay;
 use f3::hal::prelude::*;
 use f3::hal::stm32f30x;
+use f3::hal::i2c::I2c;
 use f3::led::Led;
+
 use rt::ExceptionFrame;
+
+// MPL3115A2 slave address
+const MPL3115A2_I2C_ADDR = 0xC0_u8;
 
 entry!(main);
 
@@ -24,18 +28,29 @@ fn main() -> ! {
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
+    // For LEDs
     let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+
+    // For i2c1
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
 
     // clock configuration using the default settings (all clocks run at 8 MHz)
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
     // TRY this alternate clock configuration (all clocks run at 16 MHz)
     // let clocks = rcc.cfgr.sysclk(16.mhz()).freeze(&mut flash.acr);
 
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
     let mut led: Led = gpioe
         .pe9
         .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
         .into();
     let mut delay = Delay::new(cp.SYST, clocks);
+
+    // Let's take a barometric reading!
+    
 
     loop {
         led.on();
